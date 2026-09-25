66d540367062c5514d41695a34144dc48f92c3b8
Fahimulislamfahim <fahimul42@gmail.com>
2026-09-26 02:31:30 +0600

    loginPage skeleton added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
-
+import LoginPage from './pages/LoginPage.jsx'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div>
+      <div className="header">
+      <h1 style={{ textAlign: 'center', margin: '0 auto',display: 'block', flexDirection: 'column', gap: '10px' }}>Welcome to Co_Ride</h1>
+      </div>
+      <LoginPage />
+    </div>
   )
 }
 
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -2,7 +2,15 @@ import React, { useState } from 'react'
 
 const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+        <div className="login-page">
+            <div>Log In To You Co_Ride Account</div>
+            <form>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Password" />
+                <button type="submit">Log In</button>
+                <button type="button">Sign Up</button>
+            </form>
+        </div>
     )
 }
 
